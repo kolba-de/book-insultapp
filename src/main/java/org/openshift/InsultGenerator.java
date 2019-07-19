a51a1bfb75d9230d1dae61231a46321e983b7592
kolba-de <53080079+kolba-de@users.noreply.github.com>
2019-07-19 15:50:54 +0200

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,3 +1,4 @@
+/*
 package org.openshift;
 
 import java.util.Random;
@@ -17,3 +18,48 @@ public class InsultGenerator {
 	}
 
 }
+*/
+package org.openshift;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class InsultGenerator {
+	public String generateInsult() {
+		String vowels = "AEIOU";
+		String article = "an";
+		String the Insult = "";
+		
+		try {
+			String database_URL = "jdbc:postgresql://";
+			database_URL += System.getenv("POSTGRESQL_SERVICE_HOST");
+			database_URL += "/" + System.getenv("POSTGRESQL_DATABASE");
+			
+			String username = System.getenv("POSTGRESQL_USER");
+			String password = System.getenv("PGPASSWORD");
+			Connection connection = DriverManager.getConnection(databaseURL, username, password);
+			
+			if (connection != null) {
+				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a, long_adjective b, noun c ORDER BY random () limit 1";
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL);
+				while (rs.next()) {
+					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
+						article = "a";
+					}
+					theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+				}
+				rs.close();
+				connection.close();
+			}
+		} catch (Exception e) {
+			return "Database connection problem!";
+		}
+	}
+	
+					
+							
+							
+							
